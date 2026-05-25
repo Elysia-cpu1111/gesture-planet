5c3864f272ee77b0c27ab6226b13b906fb7ea8d1
Elysia-cpu1111 <elysia.cpu1111@gmail.com>
2026-05-25 16:23:24 +0800

    添加 GitHub Pages base 路径

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/gesture-planet/',
   plugins: [react()],
   server: { host: '0.0.0.0', port: 5173, watch: { usePolling: true, interval: 1000 } },
   optimizeDeps: { exclude: ['@mediapipe/tasks-vision'] },
